9610e0be429a4de6b168d8eceb01968f2315df9a
Aristides <aristides@pluxcustoms.nl>
2021-08-22 21:13:52 +0300

    fixes

--- src/views/background-view.ts
+++ src/views/background-view.ts
@@ -43,27 +43,47 @@ class ViewBackground extends HTMLElement {
             margin-top: 6%;
         }
 
-        :host .background-circle-container label {
+        :host .background-circle-container div label {
             color: var(--pw-text-light);
             transition: color 1s ease;
             font-family: 'Roboto', sans-serif;
             font-size: 23px;
             margin-left: 10%;
         }
+
+        :host .background-circle-container div .darkerLabel {
+          filter: brightness(0.5);
+        }
+
+        :host .background-circle-container div {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          width: 57%;
+        }
     </style> 
 
     <div class="container">
       <div class="background-circle-container">
         <pw-background-component src="./src/assets/pictures/pluxbox_new.png" style = '--background-color:#03A7F1;'></pw-background-component>
-        <label> Web Developer at Pluxbox </label>
+        <div>
+          <label> Web Developer at Pluxbox </label>
+          <label class="darkerLabel"> (April 2021 - Now) </label>
+        </div>
     </div>
       <div class="background-circle-container">
         <pw-background-component src="./src/assets/pictures/pluxbox_old.png" style = '--background-color:#DBDFE6;'></pw-background-component>
-        <label>Web Developer at Pluxbox</label>
+        <div>
+          <label> Intern Web Developer at Pluxbox </label>
+          <label class="darkerLabel"> (October 2020 - April 2021) </label>
+        </div>
       </div>
       <div class="background-circle-container">
         <pw-background-component src="./src/assets/pictures/UoC.png" style = '--background-color:#DBDFE6;'></pw-background-component>
-        <label>Web Developer at Pluxbox</label>
+        <div>
+          <label> Computer Science Department, University of Crete</label>
+          <label class="darkerLabel"> (September 2015 - September 2020) </label>
+        </div>
       </div>
     </div>
       
